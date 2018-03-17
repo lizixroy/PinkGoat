8a9c9d861590060c20b8c979007036c5c7c64d7a
Roy Li <roy.li.zixuan@gmail.com>
2018-03-17 14:55:20 -0700

    Initial Commit

---
+++ PinkGoat/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PinkGoat
+//
+//  Created by Roy Li on 3/17/18.
+//  Copyright © 2018 Roy Li. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
